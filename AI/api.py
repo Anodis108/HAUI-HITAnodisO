03ddf5cc0280269fea1b3b16a1ab52c75cfae084
Anodis108 <dong10082003@gmail.com>
2024-11-16 00:19:36 +0700

    chore(gitignore): update .gitignore

--- AI/api.py
+++ AI/api.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
 from fastapi import FastAPI, UploadFile, File
 from fastapi.responses import FileResponse
 import fitz  # PyMuPDF
